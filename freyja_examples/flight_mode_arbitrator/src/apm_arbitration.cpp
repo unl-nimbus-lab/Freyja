cf484b93b3d4fea81d08cf3b0d8b022e46151737
Ajay <ajshank@users.noreply.github.com>
2022-05-16 14:09:54 +0100

    add flight mode arbitration; move examples into subdir
    * examples still not ported
    * arbitration node that switches various custom flight "modes"
    *   -- shows how to use FreyjaInternalStatus
    *   -- auto arming, takeoff and landing with Freyja

---
+++ freyja_examples/flight_mode_arbitrator/src/apm_arbitration.cpp
@@ -0,0 +1,255 @@
+#include "apm_arbitration.hpp"
+
+/* *************************
+        RC MAPPING
+  aux1 ---> pilot has given computer full authority
+  aux2 ---> pilot has requested a software landing
+  aux3 ---> pilot has put vehicle into LAND mode (hardware/AP)
+  aux4 ---> 
+* **************************
+*/
+
+ApmModeArbitrator::ApmModeArbitrator() : Node( ROS_NODE_NAME )
+{
+  declare_parameter<double>( "init_hover_pd", -0.75 );
+  declare_parameter<double>( "arm_takeoff_delay", 4.0 );
+  declare_parameter<double>( "mission_wdg_timeout", 1.0 );
+
+  loadParameters();
+  
+  e_landing_ = false;
+  arm_req_sent_ = false;
+  target_state_avail_ = false;
+  t_clock_ = 0.0;
+
+  curstate_sub_ = create_subscription<CurrentState>( "current_state", 1, 
+                            std::bind(&ApmModeArbitrator::currentStateCallback,this,_1) );
+  tgtstate_sub_ = create_subscription<ReferenceState>( "target_state", 1,
+                            std::bind(&ApmModeArbitrator::targetStateCallback, this,_1) );                            
+  fstatus_sub_ = create_subscription<FreyjaIntStatus>( "freyja_internal_status", 1,
+                            std::bind(&ApmModeArbitrator::freyjaStatusCallback, this,_1) );
+  refstate_pub_ = create_publisher<ReferenceState>( "reference_state", 1 );                            
+
+  arming_client_ = create_client<MavrosArming> ("mavros/cmd/arming" );
+
+  elanding_serv_ = create_service<Trigger>( "blind_software_landing",
+                            std::bind(&ApmModeArbitrator::eLandingServiceHandler, this, _1, _2) );
+  arbitrator_rate_ = 10.0;   // mode arbitrator runs at this rate, hz
+  manager_timer_ = create_timer( this, get_clock(),
+                            std::chrono::duration<float>(1.0/arbitrator_rate_),
+                            std::bind(&ApmModeArbitrator::manager, this) );
+  t_manager_init_ = now();
+}
+
+void ApmModeArbitrator::loadParameters()
+{
+  takeoff_spd_ = 0.2;                               // m/s
+  get_parameter( "init_hover_pd", init_hover_pd_ ); // hover here at first takeoff
+  get_parameter( "arm_takeoff_delay", ARM_TAKEOFF_DELAY );
+  get_parameter( "mission_wdg_timeout", MISSION_WDG_TIMEOUT );
+}
+
+/*
+    "target_state" : Callback for external policy controllers.
+*/
+void ApmModeArbitrator::targetStateCallback( const ReferenceState::ConstSharedPtr msg )
+{
+  target_state_avail_ = true;
+  t_tgtstate_update_ = t_clock_;
+  //incoming_ref_ = *msg;
+  if( forward_ref_state_ )
+    refstate_pub_->publish( *msg );
+}
+
+void ApmModeArbitrator::sendMavrosArmCommand( const bool req )
+{
+  auto arm_req = std::make_shared<MavrosArming::Request> ();
+  arm_req->value = req;
+  arming_client_->async_send_request( arm_req );
+}
+
+void ApmModeArbitrator::eLandingServiceHandler( const Trigger::Request::SharedPtr rq,
+                                                const Trigger::Response::SharedPtr rp )
+{
+  RCLCPP_WARN( get_logger(), "** LAND REQUEST RECV **" );
+  mission_mode_ = MissionMode::E_LANDING;
+  rp->success = true;
+}
+
+void ApmModeArbitrator::freyjaStatusCallback( const FreyjaIntStatus::ConstSharedPtr msg )
+{
+  /* msg contains vehicle mode, armed?, and rcdata */
+  
+  // in comp ctrl but not armed
+  if( msg->armed == false && msg->computer_ctrl )
+  {
+    // we are here in the beginning or in the end
+    if( mission_mode_ == MissionMode::MISSION_END )
+    {
+      // DO NOTHING: keep mission mode the same
+      mission_mode_ = MissionMode::MISSION_END;
+      vehicle_mode_ = VehicleMode::DISARMED_COMP;
+    }
+    else
+    {
+      // change to mission arm mode
+      mission_mode_ = MissionMode::PENDING_PILOT;
+      vehicle_mode_ = VehicleMode::DISARMED_COMP;
+      RCLCPP_INFO( get_logger(), "Changing mission-mode to: ARM_NOW." );
+    }
+  }
+
+  if( msg->armed && msg->computer_ctrl )
+  {
+    vehicle_mode_ = VehicleMode::ARMED_COMP;
+  }
+
+  if( msg->armed && msg->computer_ctrl == false )
+  {
+    vehicle_mode_ = VehicleMode::ARMED_NOCOMP;
+    mission_mode_ = MissionMode::PENDING_PILOT;
+  }
+}
+
+void ApmModeArbitrator::manager()
+{
+  /* Main mode manager function, called at a fixed rate */
+
+  t_clock_ = (now() - t_manager_init_).seconds();
+
+  switch( mission_mode_ )
+  {
+    case MissionMode::PENDING_PILOT :
+      {
+        RCLCPP_INFO_THROTTLE( get_logger(), *(get_clock()), 2000, "Awaiting Pilot Switch .." );
+        // send arming command and don't send again
+        if( vehicle_mode_ == VehicleMode::DISARMED_COMP )
+        {
+          if( !arm_req_sent_ )
+          {
+            sendMavrosArmCommand( true );
+            t_comp_armed_ = t_clock_;
+            arm_req_sent_ = true;
+          }
+        }
+        else if( vehicle_mode_ == VehicleMode::ARMED_COMP )
+        {
+          if( arm_req_sent_ )
+          {
+            arming_ned_ = pos_ned_;
+            updateManagerRefNED( arming_ned_ );
+            arm_req_sent_ = false;
+            mission_mode_ = MissionMode::TAKING_OFF;
+            RCLCPP_WARN( get_logger(), "-- ARMED BY COMPUTER -- TAKING OFF --" );
+          }
+          else
+          {
+            // we didn't send arming, but we find ourselves already armed
+            t_comp_armed_ = -100.0;             // we don't know when armed            
+            updateManagerRefNED( pos_ned_ );
+            mission_mode_ = MissionMode::TAKING_OFF;
+          }
+        }
+        forward_ref_state_ = false;
+        break;
+      }
+
+    case MissionMode::TAKING_OFF :
+      { 
+        // wait for a bit before actually taking off
+        if( (t_clock_ - t_comp_armed_) < ARM_TAKEOFF_DELAY )
+        {
+          RCLCPP_WARN_THROTTLE( get_logger(), *(get_clock()), 900, "Prepare for takeoff .." );
+          break;
+        }
+        // push reference state up until takeoff is done
+        if( pos_ned_(2) > init_hover_pd_ )
+        {
+          manager_refstate_.pd -= (takeoff_spd_*1.0/arbitrator_rate_);
+          manager_refstate_.vd = -takeoff_spd_;
+        }
+        else
+        {
+          // altitude ok, switch to hover mode
+          manager_refstate_.vd = 0.0;
+          updateManagerRefNED( pos_ned_ );
+          RCLCPP_INFO( get_logger(), "Done takeoff." );
+          mission_mode_ = MissionMode::HOVERING;
+        }
+        refstate_pub_->publish( manager_refstate_ );
+        forward_ref_state_ = false;
+        break;
+      }
+    case MissionMode::HOVERING :
+      {
+        // wait here until a target_state is available (from policy/rvo3 etc)
+        if( target_state_avail_ )
+          mission_mode_ = MissionMode::POLICY_EXEC;
+
+        // publish zero velocities; positions are captured at other places
+        manager_refstate_.vn = manager_refstate_.ve = manager_refstate_.vd = 0.0;
+        refstate_pub_->publish( manager_refstate_ );
+        forward_ref_state_ = false;
+        break;
+      }
+
+    case MissionMode::E_LANDING :
+      {
+        forward_ref_state_ = false;
+        // freeze current position as reference
+        if( !e_landing_ )
+        {
+          updateManagerRefNED( pos_ned_ );
+          // zero out velocities
+          manager_refstate_.vn = manager_refstate_.ve = manager_refstate_.vd = 0.0;
+          refstate_pub_->publish( manager_refstate_ );
+          RCLCPP_WARN( get_logger(), " **** FREYJA LANDING! ****\n\tLocking current position .." );
+          e_landing_ = true;
+        }
+        
+        // push reference state down until sub-arming altitude
+        if( pos_ned_(2) < arming_ned_(2) )
+        {
+          manager_refstate_.pd += (takeoff_spd_*1.0/arbitrator_rate_);
+          manager_refstate_.vd = takeoff_spd_;
+        }
+        else
+        {
+          // send disarm command and freeze
+          sendMavrosArmCommand( false );
+          mission_mode_ = MissionMode::MISSION_END;
+        }
+
+        break;
+      }
+
+    case MissionMode::POLICY_EXEC :
+      {
+        // here we simply forward incoming reference state to controller
+        forward_ref_state_ = true;
+
+        // also set a timeout for target_state updates
+        if( (t_clock_ - t_tgtstate_update_) > MISSION_WDG_TIMEOUT )
+        {
+          // capture current state, and switch to HOVER
+          updateManagerRefNED( pos_ned_ );
+          mission_mode_ = MissionMode::HOVERING;
+          target_state_avail_ = false;
+        }
+        break;
+      }
+  }
+
+  RCLCPP_INFO_THROTTLE( get_logger(), *(get_clock()), 1000,
+                        "Mission Mode: %s", MissionModeName[(int)mission_mode_] );
+
+}
+
+int main( int argc, char** argv )
+{
+  rclcpp::init( argc, argv );
+  rclcpp::spin( std::make_shared<ApmModeArbitrator>() );
+
+  rclcpp::shutdown();
+  return 0;
+}
---
+++ freyja_examples/flight_mode_arbitrator/src/apm_arbitration.hpp
@@ -0,0 +1,140 @@
+#include <iostream>
+
+#include "rclcpp/rclcpp.hpp"
+#include "mavros_msgs/msg/attitude_target.hpp"
+#include "mavros_msgs/msg/state.hpp"
+#include "mavros_msgs/msg/rc_in.hpp"
+#include "mavros_msgs/srv/command_bool.hpp"
+
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2/impl/utils.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include "geometry_msgs/msg/quaternion.hpp"
+
+#include "std_srvs/srv/set_bool.hpp"
+#include "std_srvs/srv/trigger.hpp"
+
+#include "freyja_msgs/msg/ctrl_command.hpp"
+#include "freyja_msgs/msg/freyja_internal_status.hpp"
+#include "freyja_msgs/msg/current_state.hpp"
+#include "freyja_msgs/msg/reference_state.hpp"
+
+#include "eigen3/Eigen/Dense"
+
+typedef freyja_msgs::msg::FreyjaInternalStatus  FreyjaIntStatus;
+typedef freyja_msgs::msg::CurrentState          CurrentState;
+typedef freyja_msgs::msg::ReferenceState        ReferenceState;
+typedef mavros_msgs::srv::CommandBool           MavrosArming;
+typedef std_srvs::srv::Trigger                  Trigger;
+
+using std::placeholders::_1;
+using std::placeholders::_2;
+
+#define ROS_NODE_NAME "apm_arbitration"
+
+/* *************************
+        RC MAPPING
+  aux1 ---> pilot has given computer full authority
+  aux2 ---> pilot has requested a software landing
+  aux3 ---> pilot has put vehicle into LAND mode (hardware/AP)
+  aux4 ---> 
+* **************************
+*/
+
+#define vModes(OP) \
+  OP(NO_CONNECT) \
+  OP(ARMED_NOCOMP) \
+  OP(DISARMED_COMP) \
+  OP(ARMED_COMP) \
+  OP(PILOT_CTRL) \
+  OP(COMPUTER_CTRL) \
+  OP(ARMED) \
+  OP(ARMED_BY_PILOT) \
+  OP(TAKEOFF) \
+  OP(TAKEOFF_BY_PILOT) \
+  OP(HOVER_READY) \
+  OP(LANDING) \
+  OP(DISARMED)
+
+#define mModes(OP) \
+  OP(PENDING_PILOT) \
+  OP(TAKING_OFF) \
+  OP(HOVERING) \
+  OP(E_LANDING) \
+  OP(POLICY_EXEC) \
+  OP(MISSION_END)
+
+#define MAKE_ENUM(OP) OP,
+#define MAKE_ENUM_STRS(OP) #OP,
+
+enum class VehicleMode: int
+{ vModes(MAKE_ENUM) };
+
+enum class MissionMode: int
+{ mModes(MAKE_ENUM) };
+
+const char* const VehicleModeName[] = { vModes(MAKE_ENUM_STRS) };
+const char* const MissionModeName[] = { mModes(MAKE_ENUM_STRS) };
+
+
+class ApmModeArbitrator : public rclcpp::Node
+{
+  VehicleMode vehicle_mode_;
+  MissionMode mission_mode_;
+
+  Eigen::Vector3d pos_ned_;         // current state 
+  Eigen::Vector3d arming_ned_;
+  ReferenceState incoming_ref_;     // policy's references
+  ReferenceState manager_refstate_; // manager's reference state
+
+  float init_hover_pd_;
+  float ARM_TAKEOFF_DELAY;          // wait after arming
+  float MISSION_WDG_TIMEOUT;        // wdog timeout to switch back to hover
+  float takeoff_spd_;               // rate of ascent (scalar)
+
+  bool e_landing_;                  // in emergency landing [Freyja-controlled]
+  bool arm_req_sent_;               // arming sent
+  bool forward_ref_state_;          // should we simply fwd incoming reference (policy mode)
+  bool target_state_avail_;         // has ext. policy/mission provided a reference yet
+
+  float t_clock_;                   // Node time-keeping (zero at constructor)
+  float t_comp_armed_;
+  float t_tgtstate_update_;
+  rclcpp::Time t_manager_init_;
+  
+  float arbitrator_rate_;
+  rclcpp::TimerBase::SharedPtr manager_timer_;
+
+  protected:
+    void sendMavrosArmCommand( const bool );
+    void loadParameters();
+  
+  public:
+    ApmModeArbitrator();
+    void manager();
+
+    rclcpp::Subscription<FreyjaIntStatus>::SharedPtr fstatus_sub_;
+    void freyjaStatusCallback( const FreyjaIntStatus::ConstSharedPtr );
+
+    rclcpp::Subscription<CurrentState>::SharedPtr curstate_sub_;
+    inline void currentStateCallback( const CurrentState::ConstSharedPtr msg )
+    { pos_ned_ = Eigen::Map<const Eigen::Vector3d>( msg->state_vector.data() ); }
+
+    rclcpp::Subscription<ReferenceState>::SharedPtr tgtstate_sub_;
+    void targetStateCallback( const ReferenceState::ConstSharedPtr );
+
+    rclcpp::Client<MavrosArming>::SharedPtr arming_client_;
+
+    rclcpp::Publisher<ReferenceState>::SharedPtr refstate_pub_;
+
+    rclcpp::Service<Trigger>::SharedPtr elanding_serv_;
+    void eLandingServiceHandler( const Trigger::Request::SharedPtr,
+                                 const Trigger::Response::SharedPtr );
+
+    inline void updateManagerRefNED( const Eigen::Vector3d &ned )
+    {
+      manager_refstate_.pn = ned(0);
+      manager_refstate_.pe = ned(1);
+      manager_refstate_.pd = ned(2);
+    }
+};
