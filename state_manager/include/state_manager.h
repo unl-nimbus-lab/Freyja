c6edac86a4f1c24934e8bfb9bb03259c4fd3a188
J. Blumenkamp <jb2270@cam.ac.uk>
2023-08-16 18:22:40 +0100

    Fix import for ros2 humble

--- state_manager/include/state_manager.h
+++ state_manager/include/state_manager.h
@@ -20,7 +20,7 @@
 
 #include <tf2/LinearMath/Quaternion.h>
 #include <tf2/impl/utils.h>
-#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 #include <tf2_ros/buffer.h>
 #include <tf2_ros/transform_listener.h>
 
