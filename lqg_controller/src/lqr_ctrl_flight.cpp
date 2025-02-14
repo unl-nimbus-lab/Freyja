0fe63b4357d5218c7eb6fc2da37ad68109a93cde
Ajay <ajshank@users.noreply.github.com>
2025-02-14 10:57:48 +0000

    minor code refactor, remove redundant alloc related to 27fdea4

--- lqg_controller/src/lqr_ctrl_flight.cpp
+++ lqg_controller/src/lqr_ctrl_flight.cpp
@@ -274,12 +274,11 @@ void LQRController::computeFeedback( )
   double T;
   static Eigen::Matrix<double, 4, 1> control_input;
   static Eigen::Matrix<double, 7, 1> state_err;
-  
+  static Eigen::Matrix<double, 3, 1> f_ext_applied;
+
   bool state_valid = true;
   auto tnow = now();
 
-  Eigen::Matrix<double, 3, 1> f_external = f_ext_;
-  
   /* Check the difference from last state update time */
   float state_age = (tnow - last_state_update_t_).seconds();
   if( state_age > STATEFB_MISSING_INTRV_ )
@@ -309,7 +308,8 @@ void LQRController::computeFeedback( )
     }
 
     /* Correct for external forces */
-    control_input.head<3>() -= (apply_extf_corr_ * f_external.head<3>() );
+    f_ext_applied = static_cast<double>(apply_extf_corr_) * f_ext_;
+    control_input.head<3>() -= f_ext_applied.head<3>();
   
     /* Thrust */
     T = total_mass_ * control_input.head<3>().norm();
@@ -334,7 +334,7 @@ void LQRController::computeFeedback( )
 
   /* Tell bias estimator about new control input */
   if( bias_compensation_req_ )
-    bias_est_.setControlInput( control_input, f_external );
+    bias_est_.setControlInput( control_input, f_ext_applied );
     
   /* Update the total flying mass if requested */
   if( enable_dyn_mass_estimation_ )
